0d402c88ea3bc6d9eb1520db79ec046a851dbe9b
noel <noelgerard.ryan@gmail.com>
2021-06-28 15:19:51 +0100

    removing template coding from app.js, iporting firebase and firebase store, connecting firebase api to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import firebase from 'firebase';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB5CzkbA-FFLEAZ1IM83W2acAHcoc_LQV8",
+  authDomain: "tackchat-0001.firebaseapp.com",
+  projectId: "tackchat-0001",
+  storageBucket: "tackchat-0001.appspot.com",
+  messagingSenderId: "1073979150190",
+  appId: "1:1073979150190:web:75b24b9c2d3d577feb7b7f"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
